74b966eaf2bee32934f4057d8c866685bb252932
dandiez <47832466+dandiez@users.noreply.github.com>
2023-10-27 23:50:54 +0200

    process gameresources

--- src/cws_insights/write_gifts_table.py
+++ src/cws_insights/write_gifts_table.py
@@ -1,16 +1,23 @@
 import os.path
 import cws_insights
+from cws_insights.read_files import read_all
 
 if __name__=="__main__":
-    text = """
+    this_dir = os.path.dirname(__file__)
+    extensions_to_consider = (".meta", ".lang")
+    gameresources_dir = os.path.join(this_dir, "..", "..", "gameresources")
+    all_data = read_all(gameresources_dir, file_suffixes=extensions_to_consider)
+    footer = "\n".join([f"Found '{len(v)}' files in '{k}'" for k, v in all_data.items()])
+    text = f"""
 # Gifts / Items
 
 |item|Alabaster|
 |-|-|
 |frog|❤️|
 |bad_stuff|👿|
+
+{footer}
 """
-    this_dir = os.path.dirname(__file__)
     with open(os.path.join(this_dir, "..", "..", "_site_src", "gifts.md"), "w", encoding="utf8") as f:
         f.write(text)
 
