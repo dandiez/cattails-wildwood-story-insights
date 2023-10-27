4763fae61b2c154dd91a9824164feb3c7e578d9a
dandiez <47832466+dandiez@users.noreply.github.com>
2023-10-28 00:11:16 +0200

    fix rel path issue

--- src/cws_insights/write_gifts_table.py
+++ src/cws_insights/write_gifts_table.py
@@ -5,7 +5,7 @@ from cws_insights.read_files import read_all
 if __name__=="__main__":
     this_dir = os.path.dirname(__file__)
     extensions_to_consider = (".meta", ".lang")
-    gameresources_dir = os.path.abspath(os.path.join(this_dir, "..", "..", "gameresources"))
+    gameresources_dir = os.path.abspath(os.path.join(this_dir, "..", "..", "cattails-wildwood-story-gameresources", "gameresources"))
     print(f"gameresources dir is '{gameresources_dir}'")
     all_data = read_all(gameresources_dir, file_suffixes=extensions_to_consider)
     footer = "\n\n".join([f"Found '{len(v)}' files in '{k}'" for k, v in all_data.items()])
